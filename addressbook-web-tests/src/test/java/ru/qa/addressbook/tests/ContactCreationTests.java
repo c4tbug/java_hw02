15bb7b846a4336628fb55edc37503b3583c11ba7
c4tbug <smirnovavv25@gmail.com>
2016-06-12 12:21:36 +0300

    Задание №15: Параметризовать тесты для создания групп и контактов

--- addressbook-web-tests/src/test/java/ru/qa/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/qa/addressbook/tests/ContactCreationTests.java
@@ -1,24 +1,47 @@
 package ru.qa.addressbook.tests;
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.qa.addressbook.model.ContactData;
 import ru.qa.addressbook.model.Contacts;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+
 public class ContactCreationTests extends TestBase {
 
-    @Test
-    public void testContactCreation() {
+    @DataProvider
+    public Iterator<Object[]> validContacts () throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contact.xml")));
+        String xml ="";
+        String line = reader.readLine();
+        while (line != null) {
+            xml += line;
+            line = reader.readLine();
+        }
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactData.class);
+        List<ContactData> contacts = (List<ContactData>)xstream.fromXML(xml);
+        return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
+
+    @Test (dataProvider = "validContacts")
+    public void testContactCreation(ContactData contact) {
         app.contact().gotoHomePage();
         Contacts before = app.contact().all();
-        ContactData contact = new ContactData().withName("TesT").withLastname("TestS").withPhone("78885521").withMail("test1@@.dd");
         app.contact().create(contact);
+        assertThat(app.contact().count(), equalTo(before.size() +1));
         Contacts after = app.contact().all();
-        assertThat(after.size(), equalTo(before.size() +1));
-
         assertThat(after, equalTo(before
                 .withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+
 
     }
 
--- addressbook-web-tests/src/test/java/ru/qa/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/qa/addressbook/tests/GroupCreationTests.java
@@ -1,7 +1,14 @@
 package ru.qa.addressbook.tests;
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.qa.addressbook.model.GroupData;
 import ru.qa.addressbook.model.Groups;
+
+import java.io.*;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -9,17 +16,31 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() {
-    Groups before = app.group().all();
-    GroupData group = new GroupData().withName("TestTT");
-    app.group().create(group);
-    Groups after = app.group().all();
-    assertThat(after.size(), equalTo(before.size() + 1));
-
-    assertThat(after, equalTo(
-            before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
-
+  @DataProvider
+  public Iterator<Object[]> validGroups () throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+      String xml ="";
+      String line = reader.readLine();
+    while (line != null) {
+        xml += line;
+      line = reader.readLine();
+    }
+      XStream xstream = new XStream();
+      xstream.processAnnotations(GroupData.class);
+      List<GroupData> groups = (List<GroupData>)xstream.fromXML(xml);
+      return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
   }
+
+  @Test (dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) {
+              Groups before = app.group().all();
+              app.group().create(group);
+              assertThat(app.group().count(), equalTo(before.size() +1));
+              Groups after = app.group().all();
+              assertThat(after, equalTo(
+                      before.withAdded(group.withId(after.stream()
+                              .mapToInt((g) -> g.getId()).max().getAsInt()))));
+
+            }
 
 }
